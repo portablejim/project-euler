f2ef8678e7d26ed975d45c64b9f092cfca19d0eb
James McKay <dcseee-github@yahoo.com.au>
2017-02-17 13:37:15 +1100

    Task 67: Cleanup

--- rust/src/bin/task67.rs
+++ rust/src/bin/task67.rs
@@ -3,8 +3,6 @@ extern crate test;
 
 use std::fs::File;
 use std::io::Read;
-use std::io::BufReader;
-use std::io::BufRead;
 
 fn get_tree(file_name: &str) -> Vec<Vec<u32>> {
     let mut full_file = Vec::new();
@@ -21,38 +19,7 @@ fn get_tree(file_name: &str) -> Vec<Vec<u32>> {
     .collect()
 }
 
-fn get_tree_b(file_name: &str) -> Vec<Vec<u32>> {
-    let f = File::open(file_name).expect("Cannot open file");
-    let file = BufReader::new(&f);
-    file.lines().map(|l| {
-        l.unwrap().split(|c| c == ' ')
-                .map(|s| s.trim().parse::<u32>().unwrap_or(0))
-                .collect::<Vec<u32>>()
-            //.expect("Send didn't work");
-    }).collect()
-    //out
-}
-
 fn longest_length(tree: Vec<Vec<u32>>) -> u32 {
-    let mut tree_sums: Vec<Vec<u32>> = Vec::new();
-    tree_sums.push(tree[tree.len() - 1].clone());
-    for row in (0..(tree.len() - 1)).rev() {
-        let mut temp: Vec<u32> = Vec::new();
-        for i in 0..(row + 1) {
-            let prev = tree_sums.last().expect("Tree sums is empty");
-            if prev[i] > prev[i + 1] {
-                temp.push(tree[row][i] + prev[i])
-            } else {
-                temp.push(tree[row][i] + prev[i + 1])
-            }
-        }
-        tree_sums.push(temp);
-    }
-    // Return or error with a shrug.
-    tree_sums.last().expect("Tree sums should not be empty ¯\\_(ツ)_/¯")[0]
-}
-
-fn longest_length_b(tree: Vec<Vec<u32>>) -> u32 {
     let mut last_sums: Vec<u32> = Vec::new();
 
     for v in tree.into_iter() {
@@ -90,19 +57,7 @@ fn bench_file_read(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_file_read_b(b: &mut test::Bencher) {
-    let mut x = Vec::new();
-    b.iter(|| x = get_tree_b("task67.txt"));
-}
-
-#[bench]
 fn bench_longest_with_file_read(b: &mut test::Bencher) {
     let mut x = 0;
     b.iter(|| x = longest_length(get_tree("task67.txt")));
-}
-
-#[bench]
-fn bench_longest_with_file_read_b(b: &mut test::Bencher) {
-    let mut x = 0;
-    b.iter(|| x = longest_length_b(get_tree("task67.txt")));
 }
