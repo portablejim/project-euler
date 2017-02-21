084aee1bc45292d2dd7f44a5cefbb5f424f89333
James McKay <dcseee-github@yahoo.com.au>
2017-02-21 20:43:19 +1100

    Cleanup and optmise 23

--- rust/src/bin/task23.rs
+++ rust/src/bin/task23.rs
@@ -1,19 +1,14 @@
-#![feature(test,step_by,collections_bound,btree_range)]
+#![feature(test,step_by)]
 extern crate test;
-
-use std::collections::BTreeSet;
-use std::collections::Bound::{Included, Unbounded};
 
 fn brute_factors(input: u32) -> u32 {
     match input {
         0 => 0,
         1 | 2 => 1,
-        n => {
+        _ => {
             1 +
             (2..(1 + (input as f32).sqrt() as u32))
                 .filter(|n| input > 0 && input % n == 0)
-                //.filter(|n| input % n == 0)
-                //.map(|n| { println!("{} {}", input, n); n })
                 .fold(0u32, |s, n| s + n + if n == input / n { 0 } else { input / n })
         }
 
@@ -26,11 +21,11 @@ fn brute(target: u32) -> Vec<u32> {
 
 fn smart(target: u32) -> Vec<u32> {
     let mut sums = Vec::with_capacity(target as usize);
-    for i in (0..target) {
+    for _ in 0..target {
         sums.push(1);
     }
 
-    for j in (2..target) {
+    for j in 2..target {
         for k in ((j * 2)..target).step_by(j) {
             sums[k as usize - 1] += j;
         }
@@ -45,12 +40,12 @@ fn non_abundant_sums(limit: u32) -> u32 {
                  .enumerate()
                  .filter_map(|(a, b)| if *b > (a + 1) as u32 { Some(1 + a as u32) } else { None })
                  .collect::<Vec<u32>>();
-    let mut answer_pool = (0..limit).collect::<BTreeSet<u32>>();
-    for outer in (0..abundant_nums.len()) {
-        for inner in (outer..abundant_nums.len()) {
+    let mut answer_pool = (0..limit).collect::<Vec<u32>>();
+    for outer in 0..abundant_nums.len() {
+        for inner in outer..abundant_nums.len() {
             let current = abundant_nums[outer] + abundant_nums[inner];
-            if current < limit && answer_pool.contains(&current) {
-                answer_pool.remove(&current);
+            if current < limit && answer_pool[current as usize] > 0 {
+                answer_pool[current as usize] = 0;
             }
         }
     }
